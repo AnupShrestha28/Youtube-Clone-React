0f0a9c7e90f3b94dbb6ee2ab084d3a0a3895c759
AnupShrestha28 <anupstha0168@gmail.com>
2023-06-23 14:25:00 +0545

    Youtube Clone app completed

---
+++ src/shared/Loader.jsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+  return (
+    <div class="load-bar">
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+    </div>
+  );
+};
+
+export default Loader;
